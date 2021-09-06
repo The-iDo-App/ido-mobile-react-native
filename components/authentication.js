c7989a0fc52b40bb947e0a2655680e1ea2df09ae
NicolleInterior <intercoeli@gmail.com>
2021-09-06 21:12:01 +0800

    layout the authentication

--- components/authentication.js
+++ components/authentication.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
 
 export default function AuthPage(){
@@ -7,6 +7,11 @@ export default function AuthPage(){
         return (
             <View style={styles.authWrapper}>
                 <Image source={require('../assets/idoWhite.png')} style={styles.idoLogo} />
+                <Text>Log In</Text>
+                <Text>Email Address</Text>
+                <TextInput style={styles.emailAddress} placeholder={"Email Address"}/>
+                <Text>Email Address</Text>
+                <TextInput style={styles.emailAddress} placeholder={"Email Address"} />
             </View>
         );
  
@@ -27,5 +32,12 @@ const styles = StyleSheet.create({
         height: 150,
         width: 150,
         resizeMode: 'contain',
+    },
+    emailAddress: {
+        marginTop: 40,
+        padding: 12,
+        width: 300,
+        backgroundColor: '#fff',
+        borderRadius: 40,
     }
 });
